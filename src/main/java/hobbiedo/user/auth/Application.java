1fadaa11c9f2f68d580283e0f205df1737aecab2
howudong <tjdvy963@naver.com>
2024-05-10 18:29:25 +0900

    init : 프로젝트 생성 및 gitignore 파일 생성

---
+++ src/main/java/hobbiedo/user/auth/Application.java
@@ -0,0 +1,14 @@
+package hobbiedo.user.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		int abc = 0;
+		SpringApplication.run(Application.class, args);
+	}
+
+}
