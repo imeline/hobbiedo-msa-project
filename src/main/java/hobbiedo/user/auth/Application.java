e7549a8cadea6940d0347375311ee003336d2c64
howudong <tjdvy963@naver.com>
2024-05-14 17:58:17 +0900

    remove: application 단에 불필요한 코드 제거

--- src/main/java/hobbiedo/user/auth/Application.java
+++ src/main/java/hobbiedo/user/auth/Application.java
@@ -7,8 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
 	public static void main(String[] args) {
-		int abc = 0;
 		SpringApplication.run(Application.class, args);
 	}
-
 }
