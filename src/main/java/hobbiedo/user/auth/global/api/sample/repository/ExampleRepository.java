86af3a06497650632b1dce1d9601b92bcc59d95d
howudong <tjdvy963@naver.com>
2024-05-17 14:36:26 +0900

    remove: 불필요한 예제코드 제거

--- src/main/java/hobbiedo/user/auth/global/api/sample/repository/ExampleRepository.java
+++
@@ -1,8 +0,0 @@
-package hobbiedo.user.auth.global.api.sample.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import hobbiedo.user.auth.global.api.sample.domain.Example;
-
-public interface ExampleRepository extends JpaRepository<Example, Long> {
-}
