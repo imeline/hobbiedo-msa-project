7ef2a072d56e58547d12c03f139b547f57db3113
howudong <tjdvy963@naver.com>
2024-05-20 14:27:08 +0900

    feat: Swagger 설명값 추가

--- src/main/java/hobbiedo/user/auth/user/presentation/AuthController.java
+++ src/main/java/hobbiedo/user/auth/user/presentation/AuthController.java
@@ -11,6 +11,8 @@ import hobbiedo.user.auth.user.application.AuthService;
 import hobbiedo.user.auth.user.converter.LoginConverter;
 import hobbiedo.user.auth.user.vo.request.LoginRequestVO;
 import hobbiedo.user.auth.user.vo.response.LoginResponseVO;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
@@ -18,10 +20,13 @@ import lombok.extern.slf4j.Slf4j;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/v1")
+@Tag(name = "Auth", description = "사용자 인증 API")
 public class AuthController {
 	private final AuthService authService;
 
 	@PostMapping("/login")
+	@Operation(summary = "사용자 로그인",
+			description = "특정 사용자가 로그인하여 Access와 Refresh 토큰을 발급합니다.")
 	public ApiResponse<LoginResponseVO> loginApi(@RequestBody LoginRequestVO loginVO) {
 		return ApiResponse.onSuccess(
 				SuccessStatus.USER_INTEGRATED_LOGIN_SUCCESS.getMessage(),
