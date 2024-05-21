a0e0838cf19f845810f715dbbd807b18a860e82d
sunju <iseonju745@gmail.com>
2024-05-21 17:05:02 +0900

    remove: response entity reativeWeb 과 충돌로 제거

--- src/main/java/hobbiedo/global/base/code/BaseErrorCode.java
+++
@@ -1,8 +0,0 @@
-package hobbiedo.global.base.code;
-import hobbiedo.global.base.dto.ErrorReasonDto;
-
-public interface BaseErrorCode {
-    ErrorReasonDto getReason();
-
-    ErrorReasonDto getReasonHttpStatus();
-}
