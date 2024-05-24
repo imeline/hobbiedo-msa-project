5607ff00ef0139ba09610cb72c627d1a729d5e20
sunju <iseonju745@gmail.com>
2024-05-24 16:43:55 +0900

    eureka client 적요

--- src/main/java/hobbiedo/global/config/WebSocketConfig.java
+++ src/main/java/hobbiedo/global/config/WebSocketConfig.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 // package hobbiedo.global.config;
 //
 // import java.util.HashMap;
@@ -31,3 +32,30 @@
 // 		return new WebSocketHandlerAdapter();
 // 	}
 // }
+=======
+package hobbiedo.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+import hobbiedo.chat.application.WebSocketChatHandler;
+import lombok.RequiredArgsConstructor;
+
+@Configuration
+@EnableWebSocket
+@RequiredArgsConstructor
+public class WebSocketConfig implements WebSocketConfigurer {
+
+	private final WebSocketChatHandler webSocketChatHandler;
+
+	@Override
+	public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+		// endpoint 설정 : /api/v1/chat/{postId}
+		// 이를 통해서 ws://localhost:9090/ws/chat 으로 요청이 들어오면 websocket 통신을 진행한다.
+		// setAllowedOrigins("*")는 모든 ip에서 접속 가능하도록 해줌
+		registry.addHandler(webSocketChatHandler, "/ws").setAllowedOrigins("*");
+	}
+}
+>>>>>>> b989396 (eureka client 적요)
--- src/main/java/hobbiedo/global/exception/ExceptionAdvice.java
+++ src/main/java/hobbiedo/global/exception/ExceptionAdvice.java
@@ -1,13 +1,21 @@
 package hobbiedo.global.exception;
 
+<<<<<<< HEAD
 import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.http.HttpStatus;
+=======
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+>>>>>>> b989396 (eureka client 적요)
 import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+<<<<<<< HEAD
 import org.springframework.web.server.ServerWebExchange;
 
 import hobbiedo.global.base.ApiResponse;
@@ -25,10 +33,33 @@ public class ExceptionAdvice {
 	@ExceptionHandler(ConstraintViolationException.class)
 	public Mono<ApiResponse<Object>> handleConstraintViolationException(
 		ConstraintViolationException ex, ServerWebExchange exchange) {
+=======
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import hobbiedo.global.ApiResponse;
+import hobbiedo.global.code.status.ErrorStatus;
+import hobbiedo.global.dto.ErrorReasonDto;
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.ConstraintViolationException;
+import lombok.extern.slf4j.Slf4j;
+
+@RestControllerAdvice
+@Slf4j
+public class ExceptionAdvice extends ResponseEntityExceptionHandler {
+
+	/**
+	 * @return DB 테이블 유효성 대한 검증 에러 응답 처리
+	 */
+	@ExceptionHandler
+	public ResponseEntity<Object> dbValidationException(ConstraintViolationException ex,
+		WebRequest request) {
+>>>>>>> b989396 (eureka client 적요)
 		String errorMessage = ex.getConstraintViolations()
 			.stream()
 			.map(ConstraintViolation::getMessage)
 			.findFirst()
+<<<<<<< HEAD
 			.orElseThrow(() -> new RuntimeException("ConstraintViolation 추출 오류 발생"));
 
 		return buildErrorResponse(ErrorStatus.VALID_EXCEPTION.getStatus(), errorMessage,
@@ -67,5 +98,81 @@ public class ExceptionAdvice {
 	private Mono<ApiResponse<Object>> buildErrorResponse(ApiResponse<Object> response,
 		HttpStatus httpStatus) {
 		return Mono.just(response);
+=======
+			.orElseThrow(() -> new RuntimeException("ConstrainViolation 추출 오류 발생"));
+
+		return handleExceptionInternalConstraint(
+			ex,
+			errorMessage,
+			HttpHeaders.EMPTY,
+			request);
+	}
+
+	@ExceptionHandler
+	public ResponseEntity<Object> handleCustomException(GeneralException ex,
+		WebRequest request) {
+		ErrorReasonDto errorReasonDto = ex.getErrorReasonHttpStatus();
+		ApiResponse<Object> body = ApiResponse.onFailure(errorReasonDto.getCode(),
+			errorReasonDto.getMessage(), null);
+		return toResponseEntity(ex, HttpHeaders.EMPTY, request, errorReasonDto.getHttpStatus(),
+			body);
+	}
+	//
+	// @ExceptionHandler(value = MethodArgumentNotValidException.class)
+	// public ResponseEntity<Object> handleMethodArgumentNotValid(
+	// 	MethodArgumentNotValidException ex, WebRequest request) {
+	// 	String errorMessage = ex.getBindingResult().getAllErrors().get(0).getDefaultMessage();
+	// 	ApiResponse<Object> body = ApiResponse.onFailure(ex.getStatusCode().toString(), errorMessage,
+	// 		null);
+	// 	return toResponseEntity(ex, HttpHeaders.EMPTY, request, HttpStatus.BAD_REQUEST, body);
+	// }
+
+	@Override
+	protected ResponseEntity<Object> handleMethodArgumentNotValid(
+		MethodArgumentNotValidException ex, HttpHeaders headers,
+		HttpStatusCode status, WebRequest request) {
+		log.info("ResponseEntity = {}", status);
+		ObjectError objectError = ex.getBindingResult().getAllErrors().get(0);
+
+		ApiResponse<Object> body = ApiResponse.onFailure(
+			objectError.getCode(),
+			objectError.getDefaultMessage(),
+			null);
+
+		return toResponseEntity(
+			ex,
+			HttpHeaders.EMPTY,
+			request,
+			HttpStatus.BAD_REQUEST,
+			body);
+	}
+
+	/**
+	 * @return @Valid 검증에 대한 에러 응답 처리
+	 */
+
+	private ResponseEntity<Object> handleExceptionInternalConstraint(
+		Exception ex, String errorMessage, HttpHeaders headers, WebRequest request) {
+
+		ApiResponse<Object> body = ApiResponse.onFailure(
+			ErrorStatus.VALID_EXCEPTION.getStatus(),
+			errorMessage,
+			null);
+
+		return toResponseEntity(ex, headers, request, ErrorStatus.VALID_EXCEPTION.getHttpStatus(),
+			body);
+	}
+
+	private ResponseEntity<Object> toResponseEntity(Exception ex, HttpHeaders headers,
+		WebRequest request,
+		HttpStatus httpStatus, ApiResponse<Object> body) {
+		return super.handleExceptionInternal(
+			ex,
+			body,
+			headers,
+			httpStatus,
+			request
+		);
+>>>>>>> b989396 (eureka client 적요)
 	}
 }
