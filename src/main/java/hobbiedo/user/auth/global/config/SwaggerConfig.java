1008cfc5dcb0e4680c3c3708ff12978f3185e99a
howudong <tjdvy963@naver.com>
2024-05-25 16:07:55 +0900

    build: 빌드를 위한 yml 설정 및 dockerFile 추가

--- src/main/java/hobbiedo/user/auth/global/config/SwaggerConfig.java
+++ src/main/java/hobbiedo/user/auth/global/config/SwaggerConfig.java
@@ -8,6 +8,7 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 
 @Configuration
 public class SwaggerConfig {
@@ -18,21 +19,22 @@ public class SwaggerConfig {
 	@Bean
 	public OpenAPI openApi() {
 		return new OpenAPI()
-				.components(new Components())
-				.addSecurityItem(new SecurityRequirement().addList(BEARER_TOKEN))
-				.components(new io.swagger.v3.oas.models.Components()
-						.addSecuritySchemes(BEARER_TOKEN, new SecurityScheme()
-								.name(BEARER_TOKEN)
-								.type(SecurityScheme.Type.HTTP)
-								.scheme(BEARER_SCHEME)
-								.bearerFormat(BEARER_FORMAT)))
-				.info(apiInfo());
+			.components(new Components())
+			.addSecurityItem(new SecurityRequirement().addList(BEARER_TOKEN))
+			.addServersItem(new Server().url("/member-service"))
+			.components(new io.swagger.v3.oas.models.Components()
+				.addSecuritySchemes(BEARER_TOKEN, new SecurityScheme()
+					.name(BEARER_TOKEN)
+					.type(SecurityScheme.Type.HTTP)
+					.scheme(BEARER_SCHEME)
+					.bearerFormat(BEARER_FORMAT)))
+			.info(apiInfo());
 	}
 
 	private Info apiInfo() {
 		return new Info()
-				.title("취미한다(DO) SpringDoc")
-				.description("SpringDoc을 사용한 Swagger UI 테스트")
-				.version("1.0.0");
+			.title("취미한다(DO) SpringDoc")
+			.description("SpringDoc을 사용한 Swagger UI 테스트")
+			.version("1.0.0");
 	}
 }
