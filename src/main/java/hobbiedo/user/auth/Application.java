7aac60adbe7a9f0b1c325714c41c96a47918c133
howudong <tjdvy963@naver.com>
2024-05-25 16:07:55 +0900

    build: 빌드를 위한 yml 설정 및 dockerFile 추가

--- src/main/java/hobbiedo/user/auth/Application.java
+++ src/main/java/hobbiedo/user/auth/Application.java
@@ -2,14 +2,19 @@ package hobbiedo.user.auth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.servers.Server;
 
 @SpringBootApplication
 @OpenAPIDefinition(
-		servers = @Server(url = "http://localhost:8080", description = "Local Test Server URL")
+	servers = {
+		@Server(url = "http://localhost:8080", description = "Local Test Server URL"),
+		@Server(url = "https://nukfra.site", description = "Deploy Test Server URL")
+	}
 )
+@EnableDiscoveryClient
 public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
