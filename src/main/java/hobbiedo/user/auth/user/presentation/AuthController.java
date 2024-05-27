b04b84e17b389bf4dd07bd8713f272b1bd5d0a2e
howudong <tjdvy963@naver.com>
2024-05-27 15:05:53 +0900

    refactor: API Prefix에 non-users 추가

--- src/main/java/hobbiedo/user/auth/user/presentation/AuthController.java
+++ src/main/java/hobbiedo/user/auth/user/presentation/AuthController.java
@@ -22,28 +22,28 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/v1")
+@RequestMapping("/v1/non-users")
 @Tag(name = "Auth", description = "사용자 인증 API")
 public class AuthController {
 	private final AuthService authService;
 
 	@PostMapping("/login")
 	@Operation(summary = "사용자 로그인",
-			description = "특정 사용자가 로그인하여 Access와 Refresh 토큰을 발급합니다.")
+		description = "특정 사용자가 로그인하여 Access와 Refresh 토큰을 발급합니다.")
 	public ApiResponse<LoginResponseVO> loginApi(@RequestBody LoginRequestVO loginVO) {
 		return ApiResponse.onSuccess(
-				SuccessStatus.USER_INTEGRATED_LOGIN_SUCCESS.getMessage(),
-				authService.login(AuthConverter.toRequestDTO(loginVO))
+			SuccessStatus.USER_INTEGRATED_LOGIN_SUCCESS.getMessage(),
+			authService.login(AuthConverter.toRequestDTO(loginVO))
 		);
 	}
 
 	@PostMapping("/re-issue")
 	@Operation(summary = "Access Token 재발급",
-			description = "Refresh Token이 만료되지 않았다면, Access Token과 Refresh Token을 재발급합니다.")
+		description = "Refresh Token이 만료되지 않았다면, Access Token과 Refresh Token을 재발급합니다.")
 	public ApiResponse<ReIssueResponseVO> reIssueApi(@RequestBody @Valid ReIssueRequestVO reIssueVO) {
 		return ApiResponse.onSuccess(
-				SuccessStatus.REISSUE_TOKEN_SUCCESS.getMessage(),
-				authService.reIssueToken(AuthConverter.toRequestDTO(reIssueVO))
+			SuccessStatus.REISSUE_TOKEN_SUCCESS.getMessage(),
+			authService.reIssueToken(AuthConverter.toRequestDTO(reIssueVO))
 		);
 	}
 }
