c9bb2be294622d22431ba58c90c78a1edd458f34
sunju <iseonju745@gmail.com>
2024-05-27 16:49:20 +0900

    feat: 기본 채팅 전송, 조회 기능 구현

--- src/main/java/hobbiedo/global/exception/GeneralException.java
+++ src/main/java/hobbiedo/global/exception/GeneralException.java
@@ -1,15 +1,8 @@
-<<<<<<<< HEAD:src/main/java/hobbiedo/global/exception/GeneralException.java
 package hobbiedo.global.exception;
 
 
 import hobbiedo.global.base.code.BaseErrorCode;
 import hobbiedo.global.base.dto.ErrorReasonDTO;
-========
-package hobbiedo.global.base.exception;
-
-import hobbiedo.global.base.code.BaseErrorCode;
-import hobbiedo.global.base.dto.ErrorReasonDto;
->>>>>>>> add8ffc (init: response entity 와 git issue, pr templete 세팅):src/main/java/hobbiedo/global/base/exception/GeneralException.java
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
