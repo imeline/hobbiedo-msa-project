6e23a8baadea23b4fdbb45a3a7cc78f7c7abd3ca
sunju <iseonju745@gmail.com>
2024-06-04 00:12:10 +0900

    feat: websocket 제거 및 마지막 읽은 채팅 시간 수정 기능 구현

--- src/main/java/hobbiedo/chat/infrastructure/ChatRepository.java
+++ src/main/java/hobbiedo/chat/infrastructure/ChatRepository.java
@@ -5,10 +5,12 @@ import java.time.Instant;
 import org.springframework.data.mongodb.repository.Query;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 import org.springframework.data.mongodb.repository.Tailable;
+import org.springframework.stereotype.Repository;
 
 import hobbiedo.chat.domain.Chat;
 import reactor.core.publisher.Flux;
 
+@Repository
 public interface ChatRepository extends ReactiveMongoRepository<Chat, String> {
 	@Tailable
 	@Query(value = "{ 'crewId' : ?0, 'createdAt' : { $gte: ?1 } }", fields = "{ 'id': 0 }")
--- src/main/java/hobbiedo/global/config/WebSocketConfig.java
+++ src/main/java/hobbiedo/global/config/WebSocketConfig.java
@@ -1,25 +1,33 @@
 // package hobbiedo.global.config;
 //
-// import org.springframework.context.annotation.Configuration;
-// import org.springframework.web.socket.config.annotation.EnableWebSocket;
-// import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
-// import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+// import java.util.HashMap;
+// import java.util.Map;
 //
-// import hobbiedo.chat.application.WebSocketChatHandler;
-// import lombok.RequiredArgsConstructor;
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.context.annotation.Configuration;
+// import org.springframework.web.reactive.HandlerMapping;
+// import org.springframework.web.reactive.handler.SimpleUrlHandlerMapping;
+// import org.springframework.web.reactive.socket.WebSocketHandler;
+// import org.springframework.web.reactive.socket.server.support.WebSocketHandlerAdapter;
+//
+// import hobbiedo.chat.socketHander.ChatWebSocketHandler;
 //
 // @Configuration
-// @EnableWebSocket
-// @RequiredArgsConstructor
-// public class WebSocketConfig implements WebSocketConfigurer {
+// public class WebSocketConfig {
 //
-// 	private final WebSocketChatHandler webSocketChatHandler;
+// 	@Bean
+// 	public HandlerMapping webSocketMapping(ChatWebSocketHandler chatWebSocketHandler) {
+// 		Map<String, WebSocketHandler> map = new HashMap<>();
+// 		map.put("/ws/chat", chatWebSocketHandler);
 //
-// 	@Override
-// 	public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-// 		// endpoint 설정 : /api/v1/chat/{postId}
-// 		// 이를 통해서 ws://localhost:9090/ws/chat 으로 요청이 들어오면 websocket 통신을 진행한다.
-// 		// setAllowedOrigins("*")는 모든 ip에서 접속 가능하도록 해줌
-// 		registry.addHandler(webSocketChatHandler, "/ws").setAllowedOrigins("*");
+// 		SimpleUrlHandlerMapping handlerMapping = new SimpleUrlHandlerMapping();
+// 		handlerMapping.setOrder(10);
+// 		handlerMapping.setUrlMap(map);
+// 		return handlerMapping;
+// 	}
+//
+// 	@Bean
+// 	public WebSocketHandlerAdapter handlerAdapter() {
+// 		return new WebSocketHandlerAdapter();
 // 	}
 // }
