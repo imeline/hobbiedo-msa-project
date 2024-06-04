777eaadf9e8c83b7f5419385e489d2087979dc6e
sunju <iseonju745@gmail.com>
2024-06-04 10:28:03 +0900

    build: @SpringBootTest 제거

--- src/test/java/hobbiedo/chat/ChatApplicationTests.java
+++ src/test/java/hobbiedo/chat/ChatApplicationTests.java
@@ -1,24 +1,12 @@
 package hobbiedo.chat;
 
-import java.net.URI;
-
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.web.reactive.socket.WebSocketMessage;
-import org.springframework.web.reactive.socket.client.ReactorNettyWebSocketClient;
 
-import reactor.core.publisher.Mono;
-
-@SpringBootTest
+//@SpringBootTest
 class ChatApplicationTests {
 
-	public static void main(String[] args) {
-		ReactorNettyWebSocketClient client = new ReactorNettyWebSocketClient();
-		client.execute(URI.create("ws://localhost:8080/ws/chat"), session ->
-			session.send(Mono.just(session.textMessage("{\"type\": \"auth\", \"uuid\": \"your-uuid\"}")))
-				.thenMany(session.receive().map(WebSocketMessage::getPayloadAsText).doOnNext(System.out::println))
-				.then()
-		).block();
+	@Test
+	void contextLoads() {
 	}
 
 }
