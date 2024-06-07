255d9f57a25922ef961ec3ef0963ff14a6911437
sunju <iseonju745@gmail.com>
2024-06-07 22:35:48 +0900

    feat: SocialAuth domain 구현

---
+++ src/main/java/hobbiedo/user/auth/google/domain/SocialAuth.java
@@ -0,0 +1,39 @@
+package hobbiedo.user.auth.google.domain;
+
+import hobbiedo.user.auth.member.domain.Member;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class SocialAuth {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+	@JoinColumn(name = "member_id")
+	private Member member;
+
+	@Column(length = 50, nullable = false)
+	private String externalId;
+
+	@Column(length = 20, nullable = false)
+	private String socialType;
+}
