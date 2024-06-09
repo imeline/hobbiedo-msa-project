5b560df6ae69aeda1f06a364c48c87ec1dee7f53
sunju <iseonju745@gmail.com>
2024-06-09 12:13:38 +0900

    fix: origin rebase

--- src/main/java/hobbiedo/chat/dto/response/UnReadCountDTO.java
+++ src/main/java/hobbiedo/chat/dto/response/UnReadCountDTO.java
@@ -1,19 +1,19 @@
-package hobbiedo.chat.dto.response;
-
-import hobbiedo.chat.domain.ChatUnReadStatus;
-import lombok.Builder;
-import lombok.Getter;
-
-@Getter
-@Builder
-public class UnReadCountDTO {
-	private Long crewId;
-	private Integer unreadCount;
-
-	public static UnReadCountDTO toDTO(ChatUnReadStatus chatUnReadStatus) {
-		return UnReadCountDTO.builder()
-			.crewId(chatUnReadStatus.getCrewId())
-			.unreadCount(chatUnReadStatus.getUnreadCount())
-			.build();
-	}
-}
+// package hobbiedo.chat.dto.response;
+//
+// import hobbiedo.chat.domain.ChatUnReadStatus;
+// import lombok.Builder;
+// import lombok.Getter;
+//
+// @Getter
+// @Builder
+// public class UnReadCountDTO {
+// 	private Long crewId;
+// 	private Integer unreadCount;
+//
+// 	public static UnReadCountDTO toDTO(ChatUnReadStatus chatUnReadStatus) {
+// 		return UnReadCountDTO.builder()
+// 			.crewId(chatUnReadStatus.getCrewId())
+// 			.unreadCount(chatUnReadStatus.getUnreadCount())
+// 			.build();
+// 	}
+// }
