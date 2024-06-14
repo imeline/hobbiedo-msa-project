8e0dec69b3dcc6324af8908ebef82e10fd459c19
sunju <iseonju745@gmail.com>
2024-06-14 18:31:02 +0900

    refactor: RedisConfig 수정

--- src/main/java/hobbiedo/global/config/RedisConfig.java
+++ src/main/java/hobbiedo/global/config/RedisConfig.java
@@ -1,24 +1,52 @@
 package hobbiedo.global.config;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
-@EnableRedisRepositories(basePackages = "hobbiedo.crew.infrastructure.redis")
 @Configuration
+@EnableRedisRepositories(basePackages = "hobbiedo.crew.infrastructure.redis")
 public class RedisConfig {
-
-	@Value("${spring.data.redis.host}")
-	private String redisHost;
-
-	@Value("${spring.data.redis.port}")
-	private int redisPort;
-
 	@Bean
-	public RedisConnectionFactory redisConnectionFactory() {
-		return new LettuceConnectionFactory(redisHost, redisPort);
+	public RedisTemplate<String, Object> redisTemplate(
+		RedisConnectionFactory redisConnectionFactory) {
+		RedisTemplate<String, Object> template = new RedisTemplate<>();
+		template.setConnectionFactory(redisConnectionFactory);
+		template.setKeySerializer(new StringRedisSerializer());
+		template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+		return template;
 	}
+
+	// @Value("${spring.data.redis.host}")
+	// private String host;
+	//
+	// @Value("${spring.data.redis.port}")
+	// private int port;
+	//
+	// @Value("${spring.data.redis.password}")
+	// private String password;
+	//
+	// @Bean
+	// public RedisConnectionFactory redisConnectionFactory() {
+	// 	RedisStandaloneConfiguration redisConfig = new RedisStandaloneConfiguration();
+	// 	redisConfig.setHostName(host);
+	// 	redisConfig.setPort(port);
+	// 	redisConfig.setPassword(password);
+	// 	return new LettuceConnectionFactory(redisConfig);
+	// }
+	//
+	// @Bean
+	// public RedisTemplate<String, Object> redisTemplate() {
+	// 	RedisTemplate<String, Object> template = new RedisTemplate<>();
+	// 	template.setConnectionFactory(redisConnectionFactory());
+	// 	template.setKeySerializer(new StringRedisSerializer());
+	// 	template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+	// 	template.setHashKeySerializer(new StringRedisSerializer());
+	// 	template.setHashValueSerializer(new GenericJackson2JsonRedisSerializer());
+	// 	return template;
+	// }
 }
