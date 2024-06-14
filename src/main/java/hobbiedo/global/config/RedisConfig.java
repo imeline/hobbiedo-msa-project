0e6be70f48a54e6a5956121daa3d5cbdd5f0121d
sunju <iseonju745@gmail.com>
2024-06-14 19:18:57 +0900

    sytle: RedisConfig 주석 제거

--- src/main/java/hobbiedo/global/config/RedisConfig.java
+++ src/main/java/hobbiedo/global/config/RedisConfig.java
@@ -20,33 +20,4 @@ public class RedisConfig {
 		template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
 		return template;
 	}
-
-	// @Value("${spring.data.redis.host}")
-	// private String host;
-	//
-	// @Value("${spring.data.redis.port}")
-	// private int port;
-	//
-	// @Value("${spring.data.redis.password}")
-	// private String password;
-	//
-	// @Bean
-	// public RedisConnectionFactory redisConnectionFactory() {
-	// 	RedisStandaloneConfiguration redisConfig = new RedisStandaloneConfiguration();
-	// 	redisConfig.setHostName(host);
-	// 	redisConfig.setPort(port);
-	// 	redisConfig.setPassword(password);
-	// 	return new LettuceConnectionFactory(redisConfig);
-	// }
-	//
-	// @Bean
-	// public RedisTemplate<String, Object> redisTemplate() {
-	// 	RedisTemplate<String, Object> template = new RedisTemplate<>();
-	// 	template.setConnectionFactory(redisConnectionFactory());
-	// 	template.setKeySerializer(new StringRedisSerializer());
-	// 	template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
-	// 	template.setHashKeySerializer(new StringRedisSerializer());
-	// 	template.setHashValueSerializer(new GenericJackson2JsonRedisSerializer());
-	// 	return template;
-	// }
 }
