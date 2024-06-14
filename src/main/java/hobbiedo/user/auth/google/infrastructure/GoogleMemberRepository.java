25c857d81ce9a063ac5db6112abe7677ebedad01
sunju <iseonju745@gmail.com>
2024-06-14 21:37:55 +0900

    refactor: 구글 로그인 시, 이미 회원인지 확인 조건을 이름+이메일 -> 이메일 으로 변경

--- src/main/java/hobbiedo/user/auth/google/infrastructure/GoogleMemberRepository.java
+++ src/main/java/hobbiedo/user/auth/google/infrastructure/GoogleMemberRepository.java
@@ -7,7 +7,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import hobbiedo.user.auth.member.domain.Member;
 
 public interface GoogleMemberRepository extends JpaRepository<Member, Long> {
-	Optional<Member> findByNameAndEmail(String name, String email);
-
-	Boolean existsByEmail(String email);
+	Optional<Member> findByEmail(String email);
 }
