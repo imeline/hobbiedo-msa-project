e169dd0eb22a4003a6adbe21b6df008ae84986f2
sunju <iseonju745@gmail.com>
2024-06-14 12:26:39 +0900

    feat: 취미와 활동 지역에 해당하는 소모임 목록 조회 api 구현

--- src/main/java/hobbiedo/crew/infrastructure/CrewRepository.java
+++ src/main/java/hobbiedo/crew/infrastructure/CrewRepository.java
@@ -1,9 +1,15 @@
 package hobbiedo.crew.infrastructure;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import hobbiedo.crew.domain.Crew;
 
 public interface CrewRepository extends JpaRepository<Crew, Long> {
 
+	@Query("select c.id from Crew c where c.hobbyId = :hobbyId and c.regionId = :regionId and c.active = true")
+	List<Long> findIdsByHobbyAndRegion(long hobbyId, long regionId);
 }
