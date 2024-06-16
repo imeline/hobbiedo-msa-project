8c89c320ebfe4322aa62714448aff1169659b86a
sunju <iseonju745@gmail.com>
2024-06-17 02:39:30 +0900

    refactor: 한 유저가 가입한 소모임 프로필 목록 조회, currentParticipant 추가

--- src/main/java/hobbiedo/crew/dto/response/CrewProfileDTO.java
+++ src/main/java/hobbiedo/crew/dto/response/CrewProfileDTO.java
@@ -10,12 +10,14 @@ public class CrewProfileDTO {
 	private long crewId;
 	private String name;
 	private String profileUrl;
+	private int currentParticipant;
 
 	public static CrewProfileDTO toDto(Crew crew) {
 		return CrewProfileDTO.builder()
 			.crewId(crew.getId())
 			.name(crew.getName())
 			.profileUrl(crew.getProfileUrl())
+			.currentParticipant(crew.getCurrentParticipant())
 			.build();
 	}
 }
