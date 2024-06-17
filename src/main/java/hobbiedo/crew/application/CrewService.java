dd674954a5782003effa6802c6c6bac64160de5a
sunju <iseonju745@gmail.com>
2024-06-17 10:13:06 +0900

    refactor: service 에서 @Transactional 어노테이션 제거

--- src/main/java/hobbiedo/crew/application/CrewService.java
+++ src/main/java/hobbiedo/crew/application/CrewService.java
@@ -2,8 +2,6 @@ package hobbiedo.crew.application;
 
 import java.util.List;
 
-import org.springframework.transaction.annotation.Transactional;
-
 import hobbiedo.crew.dto.request.CrewOutDTO;
 import hobbiedo.crew.dto.request.CrewRequestDTO;
 import hobbiedo.crew.dto.request.JoinFormRequestDTO;
@@ -31,29 +29,23 @@ public interface CrewService {
 
 	CrewNameDTO getCrewName(Long crewId);
 
-	@Transactional
 	void deleteCrewMember(Long crewId, String uuid);
 
 	CrewResponseDTO getCrew(Long crewId);
 
-	@Transactional
 	void modifyCrew(CrewRequestDTO crewDTO, Long crewId, String uuid);
 
-	@Transactional
 	void forcedExitCrew(CrewOutDTO crewOutDTO, Long crewId, String uuid);
 
 	List<JoinFormListDTO> getJoinFormList(Long crewId, String uuid);
 
 	JoinFormResponseDTO getJoinForm(String joinFormId);
 
-	@Transactional
 	void acceptJoinForm(String joinFormId, String uuid);
 
-	@Transactional
 	void rejectJoinForm(String joinFormId, String uuid);
 
 	List<MyJoinFormDTO> getMyJoinForms(String uuid);
 
-	@Transactional
 	void cancelJoinForm(String joinFormId, String uuid);
 }
