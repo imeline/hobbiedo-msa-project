90c8baa1447c1a14cb9c5dd51f1f9ae9fee1f3f1
sunju <iseonju745@gmail.com>
2024-06-19 17:09:09 +0900

    feat: ChatApplication에 @EnableScheduling 추가

--- src/main/java/hobbiedo/ChatApplication.java
+++ src/main/java/hobbiedo/ChatApplication.java
@@ -2,17 +2,12 @@ package hobbiedo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
-import org.springframework.boot.autoconfigure.data.mongo.MongoReactiveDataAutoConfiguration;
-import org.springframework.boot.autoconfigure.data.mongo.MongoReactiveRepositoriesAutoConfiguration;
-import org.springframework.boot.autoconfigure.data.mongo.MongoRepositoriesAutoConfiguration;
-import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
-import org.springframework.boot.autoconfigure.mongo.MongoReactiveAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableScheduling
 public class ChatApplication {
 
 	public static void main(String[] args) {
