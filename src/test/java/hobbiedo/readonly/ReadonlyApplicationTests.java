0326f1bbf9b5f61ad79f294fdb76d80a465563cb
Kim seonuk <kswms403@naver.com>
2024-06-20 16:48:21 +0900

    fix: 빌드 오류 수정을 위한 테스트 파일 어노테이션 삭제

--- src/test/java/hobbiedo/readonly/ReadonlyApplicationTests.java
+++ src/test/java/hobbiedo/readonly/ReadonlyApplicationTests.java
@@ -1,9 +1,7 @@
 package hobbiedo.readonly;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class ReadonlyApplicationTests {
 
 	@Test
