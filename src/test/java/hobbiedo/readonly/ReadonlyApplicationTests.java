d8dad92f07c77e776e9004456f7900377a018f59
Kim seonuk <kswms403@naver.com>
2024-06-20 11:27:45 +0900

    init: 프로젝트 초기 설정 및 코드 스타일 설정, 도커파일 설정

---
+++ src/test/java/hobbiedo/readonly/ReadonlyApplicationTests.java
@@ -0,0 +1,13 @@
+package hobbiedo.readonly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReadonlyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
