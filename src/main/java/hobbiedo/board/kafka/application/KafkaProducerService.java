775019a7c711000a4f185026abc2324daddb7813
Kim seonuk <kswms403@naver.com>
2024-06-20 21:27:40 +0900

    feat: 게시글 고정 및 고정 해제 토픽 추가

--- src/main/java/hobbiedo/board/kafka/application/KafkaProducerService.java
+++ src/main/java/hobbiedo/board/kafka/application/KafkaProducerService.java
@@ -8,30 +8,38 @@ import hobbiedo.board.kafka.dto.BoardCommentUpdateDto;
 import hobbiedo.board.kafka.dto.BoardCreateEventDto;
 import hobbiedo.board.kafka.dto.BoardDeleteEventDto;
 import hobbiedo.board.kafka.dto.BoardLikeUpdateDto;
+import hobbiedo.board.kafka.dto.BoardPinEventDto;
+import hobbiedo.board.kafka.dto.BoardUnPinEventDto;
 import hobbiedo.board.kafka.dto.BoardUpdateEventDto;
 
 @Service
 public class KafkaProducerService {
 
-	// 통계 테이블 생성 이벤트용 토픽
+	// 게시글 생성 이벤트용 토픽
 	private static final String BOARD_CREATE_TOPIC = "board-create-topic";
 
-	// 통계 테이블 삭제 이벤트용 토픽
+	// 게시글 삭제 이벤트용 토픽
 	private static final String BOARD_DELETE_TOPIC = "board-delete-topic";
 
-	// 통계 테이블 수정 이벤트용 토픽
+	// 게시글 수정 이벤트용 토픽
 	private static final String BOARD_UPDATE_TOPIC = "board-update-topic";
 
-	// 통계 테이블 댓글 수 증가 이벤트용 토픽
+	// 게시글 고정 이벤트용 토픽
+	private static final String BOARD_PIN_TOPIC = "board-pin-topic";
+
+	// 게시글 고정 해제 이벤트용 토픽
+	private static final String BOARD_UNPIN_TOPIC = "board-unpin-topic";
+
+	// 게시글 댓글 수 증가 이벤트용 토픽
 	private static final String BOARD_COMMENT_UPDATE_TOPIC = "board-comment-update-topic";
 
-	// 통계 테이블 댓글 수 감소 이벤트용 토픽
+	// 게시글 댓글 수 감소 이벤트용 토픽
 	private static final String BOARD_COMMENT_DELETE_TOPIC = "board-comment-delete-topic";
 
-	// 통계 테이블 좋아요 수 증가 이벤트용 토픽
+	// 게시글 좋아요 수 증가 이벤트용 토픽
 	private static final String BOARD_LIKE_UPDATE_TOPIC = "board-like-update-topic";
 
-	// 통계 테이블 좋아요 수 감소 이벤트용 토픽
+	// 게시글 좋아요 수 감소 이벤트용 토픽
 	private static final String BOARD_LIKE_DELETE_TOPIC = "board-like-delete-topic";
 
 	@Autowired
@@ -64,6 +72,28 @@ public class KafkaProducerService {
 		try {
 
 			kafkaTemplate.send(BOARD_UPDATE_TOPIC, eventDto);
+		} catch (Exception e) {
+
+			e.printStackTrace();
+		}
+	}
+
+	// 게시글 고정 이벤트 메시지 전송
+	public void sendPinTableMessage(BoardPinEventDto eventDto) {
+		try {
+
+			kafkaTemplate.send(BOARD_PIN_TOPIC, eventDto);
+		} catch (Exception e) {
+
+			e.printStackTrace();
+		}
+	}
+
+	// 게시글 고정 해제 이벤트 메시지 전송
+	public void sendUnpinTableMessage(BoardUnPinEventDto eventDto) {
+		try {
+
+			kafkaTemplate.send(BOARD_UNPIN_TOPIC, eventDto);
 		} catch (Exception e) {
 
 			e.printStackTrace();
