846303f0e7f9ff1b1fad0ba654103751e111bd81
Kim seonuk <kswms403@naver.com>
2024-06-21 17:23:28 +0900

    refactor: 게시글 댓글, 좋아요 수 컨슈머 dto 클래스명 및 필드값 수정

--- src/main/java/hobbiedo/board/kafka/dto/BoardLikeCountUpdateDto.java
+++ src/main/java/hobbiedo/board/kafka/dto/BoardLikeCountUpdateDto.java
@@ -9,7 +9,8 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
-public class BoardLikeUpdateDto {
+public class BoardLikeCountUpdateDto {
 
 	private Long boardId;
+	private Integer likeCount;
 }
